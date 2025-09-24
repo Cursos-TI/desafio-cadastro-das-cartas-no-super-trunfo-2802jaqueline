d2ebf366ffece00f2617342f7d31907d7d8225a8
2802jaqueline <jaqueline.ferraz02@gmail.com>
2025-09-24 23:43:42 +0000

    NOVO commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,22 +7,25 @@ int main(){
           char Estado_1[30] = "Alagoas";
           char Código_1[30] = "A01";
           char NomedaCidade_1[30] = "Máceio";
-          int População_1 = 3220104;
-          float Área_1 = 27.768;
-          float PIB_1 = 760.077;
+          unsigned long int População_1 = 3220104;
+          float Área_1 = 277680.00;
+          float PIB_1 = 2760077.00;
           int PontosTuristicos_1 = 14;
-          float densidadepopulacional_1;
-          float PIBpercapita_1;
+          float densidadepopulacional_1 = (float) População_1 / Área_1;
+          float PIBpercapita_1 = (float) PIB_1 / População_1;
+          float SuperPoder_1;
+
 
           char Estado_2[30] = "Acre";
           char Código_2[30] = "B02";
           char NomedaCidade_2[30] = "Rio Branco";
-          int População_2 = 830026;
-          float Área_2 = 164.123;
-          float PIB_2 = 173.300;
+          unsigned long int População_2 = 830026;
+          float Área_2 = 164123.00;
+          float PIB_2 = 173300.00;
           int PontosTuristicos_2 = 8;
-          float densidadepopulacional_2;
-          float PIBpercapita_2;
+          float densidadepopulacional_2 = (float) População_2 / Área_2;
+          float PIBpercapita_2 = (float) PIB_2 / População_2;
+          float SuperPoder_2;
 
           printf("Estado:\n ");
           scanf("%s", Estado_1);
@@ -34,7 +37,7 @@ int main(){
           scanf("%s", NomedaCidade_1);
 
           printf("População:\n ");
-          scanf("%d", &População_1);
+          scanf("%lu", &População_1);
 
           printf("Area Total:\n ");
           scanf("%f", &Área_1);
@@ -44,6 +47,12 @@ int main(){
 
           printf("Numeros de Pontos Turisticos:\n ");
           scanf("%d", &PontosTuristicos_1);
+           
+          printf("Densidade populacional:\n ");
+          printf("%.3fhab/km2\n", densidadepopulacional_1);
+
+          printf("PIB per Capita\n ");
+          printf("%freais\n\n", PIBpercapita_1);
 
           printf("CADASTRO DA CARTA 2\n");
 
@@ -57,7 +66,7 @@ int main(){
           scanf("%s", NomedaCidade_2);
 
           printf("População:\n ");
-          scanf("%d", &População_2);
+          scanf("%lu", &População_2);
 
           printf("Area Total:\n ");
           scanf("%f", &Área_2);
@@ -67,34 +76,48 @@ int main(){
 
           printf("Numeros de Pontos Turisticos:\n ");
           scanf("%d", &PontosTuristicos_2);
+           
+          printf("densidade populacional:\n ");
+          printf("%.3fhab/km2\n", densidadepopulacional_2);
+
+          printf("PIB per Capita:\n ");
+          printf("%freais\n\n", PIBpercapita_2);
 
           printf("DADOS DA CARTA 1\n");
           printf("Estado: %s - Código da Carta: %s\n", Estado_1, Código_1);
-          printf("Nome Da Cidade: %s - População: %d\n", NomedaCidade_1, População_1);
+          printf("Nome Da Cidade: %s - População: %lu\n", NomedaCidade_1, População_1);
           printf("Área Total: %.3f - PIB: %.3f\n", Área_1, PIB_1);
           printf("Numeros de Pontos Turisticos: %d\n", PontosTuristicos_1);
-
+          densidadepopulacional_1 = (float)(População_1 / Área_1);
+          printf("Densidade populacional: %.3fhab/km2\n", densidadepopulacional_1);
+          PIBpercapita_1 = (float)(PIB_1 / População_1);
+          printf("PIB per capita: %freais\n", PIBpercapita_1);
+          SuperPoder_1 =(float) População_1 + Área_1 + PIB_1 + PontosTuristicos_1 + densidadepopulacional_1 + PIBpercapita_1;
+          printf("Super Poder carta 1: %f\n\n", SuperPoder_1);
+          
           printf("DADOS DA CARTA 2\n");
           printf("Estado: %s - Código da Carta: %s\n", Estado_2, Código_2);
-          printf("Nome Da cidade: %s - População: %d\n", NomedaCidade_2, População_2);
+          printf("Nome Da cidade: %s - População: %lu\n", NomedaCidade_2, População_2);
           printf("Área Total: %.3f - PIB: %.3f\n", Área_2, PIB_2);
-          printf("Numeros de Pontos Turisticos: %d", PontosTuristicos_2);
-
-          printf("¨¨¨¨valor densidade populacional carta 1¨¨¨¨\n");
-          densidadepopulacional_1 = (float)(População_1 / Área_1);
-          printf("A densidade populacional da carta 1 é: %f\n", densidadepopulacional_1);
-
-          printf("¨¨¨¨PIB per Capita da carta 1¨¨¨¨\n");
-          PIBpercapita_1 = (float)(PIB_1 / População_1);
-          printf("O PIB per capita da carta 1 é: %f\n", PIBpercapita_1);
-
-          printf("¨¨¨¨valor densidade populacional carta 2¨¨¨¨\n");
+          printf("Numeros de Pontos Turisticos: %d\n", PontosTuristicos_2);
           densidadepopulacional_2 = (float)(População_2 / Área_2);
-          printf("A densidade populacional da carta 2 é: %f\n", densidadepopulacional_2);
+          printf("Densidade populacional: %.3fhab/km2\n", densidadepopulacional_2);
+          PIBpercapita_2 = (float)(PIB_2 / População_2);
+          printf("PIB per capita: %freais\n", PIBpercapita_2);
+          SuperPoder_2 =(float) População_2 + Área_2 + PIB_2 + PontosTuristicos_2 + densidadepopulacional_2 + PIBpercapita_2;
+          printf("Super Poder Carta 2: %f\n\n", SuperPoder_2);
 
-          printf("¨¨¨¨PIB per Capita da carta 2¨¨¨¨\n");
-          PIBpercapita_1 = (float)(PIB_2 / População_2);
-          printf("O PIB per capita da carta 2 é: %f\n", PIBpercapita_2);
+          printf("Comparação das cartas\n ");
+          printf("População_1 > População_2: %u\n", População_1 > População_2);
+          printf("Área_1 > Área_2: %d\n", Área_1 > Área_2);
+          printf("PIB_1 > PIB_2: %d\n", PIB_1 > PIB_2);
+          printf("pontos tursiticos_1 > pontos turisticos_2: %d\n", PontosTuristicos_1 > PontosTuristicos_2);
+          printf("desidade populacional_1 < densidade populacional_2: %d\n", densidadepopulacional_1 < densidadepopulacional_2);
+          printf("pib per capita_1 > pib per capita_2: %d\n", PIBpercapita_1 > PIBpercapita_2);
+          printf("Super Poder_1 > Super Poder_2: %d", SuperPoder_1 >SuperPoder_2);
+      
+
+        
 
 
 
