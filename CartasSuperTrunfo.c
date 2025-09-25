4e03def65e87c8c6ad51a68c55a68c7ed7f8185e
2802jaqueline <jaqueline.ferraz02@gmail.com>
2025-09-25 00:15:55 +0000

    NOVO commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,25 +7,25 @@ int main(){
           char Estado_1[30] = "Alagoas";
           char Código_1[30] = "A01";
           char NomedaCidade_1[30] = "Máceio";
-          unsigned long int População_1 = 3220104;
-          float Área_1 = 277680.00;
-          float PIB_1 = 2760077.00;
+          unsigned long int População_1 = 1220104;
+          float Área_1 = 27768.00;
+          float PIB_1 = 1960077.00;
           int PontosTuristicos_1 = 14;
           float densidadepopulacional_1 = (float) População_1 / Área_1;
           float PIBpercapita_1 = (float) PIB_1 / População_1;
-          float SuperPoder_1;
+          float SuperPoder_1 = (float) População_1 + Área_1 + PIB_1 + PontosTuristicos_1 + densidadepopulacional_1 + PIBpercapita_1;
 
 
           char Estado_2[30] = "Acre";
           char Código_2[30] = "B02";
-          char NomedaCidade_2[30] = "Rio Branco";
-          unsigned long int População_2 = 830026;
-          float Área_2 = 164123.00;
-          float PIB_2 = 173300.00;
+          char NomedaCidade_2[30] = "RioBranco";
+          unsigned long int População_2 = 1230026;
+          float Área_2 = 16412.00;
+          float PIB_2 = 2073300.00;
           int PontosTuristicos_2 = 8;
           float densidadepopulacional_2 = (float) População_2 / Área_2;
           float PIBpercapita_2 = (float) PIB_2 / População_2;
-          float SuperPoder_2;
+          float SuperPoder_2 = (float) População_2 + Área_2 + PIB_2 + PontosTuristicos_2 + densidadepopulacional_2 + PIBpercapita_2;
 
           printf("Estado:\n ");
           scanf("%s", Estado_1);
@@ -88,11 +88,8 @@ int main(){
           printf("Nome Da Cidade: %s - População: %lu\n", NomedaCidade_1, População_1);
           printf("Área Total: %.3f - PIB: %.3f\n", Área_1, PIB_1);
           printf("Numeros de Pontos Turisticos: %d\n", PontosTuristicos_1);
-          densidadepopulacional_1 = (float)(População_1 / Área_1);
           printf("Densidade populacional: %.3fhab/km2\n", densidadepopulacional_1);
-          PIBpercapita_1 = (float)(PIB_1 / População_1);
           printf("PIB per capita: %freais\n", PIBpercapita_1);
-          SuperPoder_1 =(float) População_1 + Área_1 + PIB_1 + PontosTuristicos_1 + densidadepopulacional_1 + PIBpercapita_1;
           printf("Super Poder carta 1: %f\n\n", SuperPoder_1);
           
           printf("DADOS DA CARTA 2\n");
@@ -100,11 +97,8 @@ int main(){
           printf("Nome Da cidade: %s - População: %lu\n", NomedaCidade_2, População_2);
           printf("Área Total: %.3f - PIB: %.3f\n", Área_2, PIB_2);
           printf("Numeros de Pontos Turisticos: %d\n", PontosTuristicos_2);
-          densidadepopulacional_2 = (float)(População_2 / Área_2);
           printf("Densidade populacional: %.3fhab/km2\n", densidadepopulacional_2);
-          PIBpercapita_2 = (float)(PIB_2 / População_2);
           printf("PIB per capita: %freais\n", PIBpercapita_2);
-          SuperPoder_2 =(float) População_2 + Área_2 + PIB_2 + PontosTuristicos_2 + densidadepopulacional_2 + PIBpercapita_2;
           printf("Super Poder Carta 2: %f\n\n", SuperPoder_2);
 
           printf("Comparação das cartas\n ");
